46beb4c1b62869552be3b400dc3faf9c9ee259d0
agelloz <agelloz@student.42.fr>
2019-10-18 09:36:06 +0200

    day03

--- day_03/ex03/cookie_crisp.php
+++ day_03/ex03/cookie_crisp.php
@@ -1,8 +1,9 @@
 <?php
-if (isset($_GET["action"]) || isset($_GET["name"]))
+date_default_timezone_set('Europe/paris');
+if (!isset($_GET["action"]) || !isset($_GET["name"]))
 	return null;
 if ($_GET["action"] == "set")
-	setcookie($_GET["name"], $_GET["value"], time() + 3600);
+	setcookie($_GET["name"], $_GET["value"], 0, '/');
 elseif ($_GET["action"] == "get" && isset($_COOKIE[$_GET["name"]]))
 	echo $_COOKIE[$_GET["name"]] . "\n";
 elseif (($_GET["action"] == "del") && isset($_COOKIE[$_GET["name"]]))
