37159500b961722ac48d589b5fa7388c8ae061c1
agelloz <agelloz@student.42.fr>
2019-10-20 23:34:39 +0200

    second

--- rush_00/signup.php
+++ rush_00/signup.php
@@ -10,20 +10,22 @@ if ($_POST["login"] != NULL && $_POST["passwd"] != NULL
     $hashed_pwd = hash('whirlpool', $_POST["passwd"]);
     $query_check = "SELECT login FROM users WHERE login='" . $_POST["login"] . "'";
     $run = mysqli_query($con, $query_check);
-    if ($run && mysqli_num_rows($run))
-    {
-        $_SESSION["loggued_on_user"] = $_POST["login"];
-        echo "<head><link rel='stylesheet' type='text/css' href='style.css'></head><html><body><h2>Account exists already. You have been signed in :)</h2></body></html>";
-        header("Refresh: 2;url=index.php");
-    }
-    else
+    if ($run && !mysqli_num_rows($run))
     {
         $query = "INSERT INTO users SET login = '" . $_POST["login"] . "', password ='" . $hashed_pwd . "'";
         $run_pro = mysqli_query($con, $query);
         if (!$run_pro)
             die("ERROR: " . mysqli_error($con));
         header("Refresh: 2;url=login.html");
-        echo "<head><link rel='stylesheet' type='text/css' href='style.css'></head><html><body><h2>Welcome ". $_SESSION["user"] . "! Happy shopping :)</h2></body></html>";
+        echo "<head><link rel='stylesheet' type='text/css' href='style.css'></head><html><body><h2>Welcome ". $_POST["login"] . "! Happy shopping :)</h2></body></html>";
+    }
+    elseif ($run)
+    {
+        header("Refresh: 4;url=signup.html");
+        echo "<head><link rel='stylesheet' type='text/css' href='style.css'></head>
+        <html><body><h2>
+            Account already existing, please try again.<br>
+        </h2></body></html>";
     }
 }
 else
