83086acd39b3829734bf65a2014f7eb17c6fed73
agelloz <agelloz@student.42.fr>
2019-10-22 15:19:23 +0200

    day05

--- day_05/ex17/ex17.sql
+++ day_05/ex17/ex17.sql
@@ -1,2 +1,2 @@
-SELECT COUNT(id_sub) AS `nb_abo`, FLOOR(AVG(price)) AS `moy_abo`, MOD(SUM(duration_sub), 42) AS `ft` 
+SELECT COUNT(id_sub) AS `nb_abo`, FLOOR(AVG(price)) AS `moy_abo`, SUM(MOD(duration_sub, 42)) AS `ft` 
 FROM subscription;
