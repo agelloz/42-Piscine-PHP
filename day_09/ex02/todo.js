940fa366b26daaa8e6ab2952785c529e8abbe07c
agelloz <agelloz@student.42.fr>
2019-10-26 16:26:00 +0200

    day09

--- day_09/ex02/todo.js
+++ day_09/ex02/todo.js
@@ -1,91 +1,104 @@
-function createCookie(name, value, days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
+function createCookie(name, value, days)
+{
+    if (days)
+    {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        var expires = "expires="+ date.toUTCString();
+    }
+    else
+        var expires = ""; 
+    document.cookie = name + "=" + value + ";" + expires + ";path=./";
 }
 
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
+function readCookie(name)
+{
+    var name = name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) 
+    {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') 
+            c = c.substring(1);
+        if (c.indexOf(name) == 0)
+            return (c.substring(name.length, c.length));
+    }
+    return null;
 }
 
-function deleteItem() {
+function deleteItem()
+{
     var i = 0;
     var array = new Array();
 
-    var listItems = document.getElementsByClassName("item");
-    while (i < listItems.length) {
-        listItems[i].onclick = function() {
-        if (window.confirm(`Are you sure you want to delete ${this.textContent} ?`))
+    var items = document.getElementsByClassName("item");
+    while (i < items.length)
+    {
+        items[i].onclick = function() 
         {
-            this.parentNode.removeChild(this);
-            array = [];
-            for (var i = 0; i < listItems.length; i++)
-                array.push(listItems[i].innerText);
-            var cookie_list = JSON.stringify(array);
-            createCookie("todo", cookie_list);
-        }
-        else
-            return;
+            if (window.confirm(`Are you certain to delete task '${this.textContent}' ?`))
+            {
+                this.parentNode.removeChild(this);
+                array = [];
+                for (var i = 0; i < items.length; i++)
+                    array.push(items[i].innerText);
+                var cookie = JSON.stringify(array);
+                createCookie("list", cookie, 30);
+            }
+            else
+                return;
         }
         i++;
     }
 }
 
-function createItem() {
+function createItem()
+{
     var input = prompt("New task:");
-    var main_div = document.getElementById("ft_list");
+    var list = document.getElementById("ft_list");
     var array = new Array();
 
     if (input === null || input === "" || !input.trim())
         return;
     var text_node = document.createTextNode(input);
-    var new_item = document.createElement('div');
+    var new_item = document.createElement("div");
     new_item.appendChild(text_node);
     new_item.setAttribute("class", "item");
-    if (main_div.childElementCount === 0)
+    if (list.childElementCount === 0)
     {
-        main_div.appendChild(new_item);
-        var todo_list = document.getElementsByClassName('item');
-        for (var i = 0; i < todo_list.length; i++)
-            array.push(todo_list[i].innerText);
-        var cookie_list = JSON.stringify(array);
-        createCookie("todo", cookie_list);
+        list.appendChild(new_item);
+        var items = document.getElementsByClassName("item");
+        for (var i = 0; i < items.length; i++)
+            array.push(items[i].innerText);
+        var cookie = JSON.stringify(array);
+        createCookie("list", cookie, 30);
     }
     else
     {
-        main_div.insertBefore(new_item, main_div.firstChild);
-        var todo_list = document.getElementsByClassName('item');;
-        for (var i = 0; i < todo_list.length; i++)
-            array.push(todo_list[i].innerText);
-        var cookie_list = JSON.stringify(array);
-        createCookie("todo", cookie_list);
+        list.insertBefore(new_item, list.firstChild);
+        var items = document.getElementsByClassName("item");;
+        for (var i = 0; i < items.length; i++)
+            array.push(items[i].innerText);
+        var cookie = JSON.stringify(array);
+        createCookie("list", cookie, 30);
     }
     deleteItem();
 }
 
-function createTodo() {
-    if (getCookie("todo"))
+function createTodo()
+{
+    if (readCookie("list") != null)
     {
-        cookie = JSON.parse(readCookie("todo"));
-        var main_div = document.getElementById("ft_list");
-        
-        for (var i = 0; i < cookie.length; i++) {
+        cookie = JSON.parse(readCookie("list"));
+        var list = document.getElementById("ft_list");
+        for (var i = 0; i < cookie.length; i++)
+        {
             var text_node = document.createTextNode(cookie[i]);
-            var new_item = document.createElement('div');
+            var new_item = document.createElement("div");
             new_item.appendChild(text_node);
             new_item.setAttribute("class", "item");
-            main_div.appendChild(new_item);
+            list.appendChild(new_item);
         }
     }
     deleteItem();
